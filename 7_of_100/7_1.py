86529ed0bfe7e9718d9b51b83c3123bc68c984f1
DesLandysh <denis.schizarium@gmail.com>
2021-08-09 01:38:56 +0300

    Flowchart

---
+++ 7_of_100/7_1.py
@@ -0,0 +1,14 @@
+# Hangman logic
+
+# Choose the word = chosen_word
+# Draw the tree as a list of lists
+# Draw the _ _ same as length of chosen_word)
+# take input from user a letter
+# if letter is in the chosen_word
+#   then show the all matched letters in the _ _ _ _ var of word
+# if letter is not in the chosen_word
+#   then draw a piece of hangman up to 6 tries
+# and while 6 tries is not end
+#   then ask user to input the letter
+#       else game over
+
