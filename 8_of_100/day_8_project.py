c4a381dbc7b2f6a936ef764b68ad0e75d394b487
DesLandysh <denis.schizarium@gmail.com>
2021-08-10 01:38:53 +0300

    Step 1a. Fix 'zulu' and multiwords issues

--- 8_of_100/day_8_project.py
+++ 8_of_100/day_8_project.py
@@ -1,16 +1,19 @@
 # Caesar Cipher
 
-def encrypt(base, txt, code):
+def encrypt(txt, code):
     cipher_txt = ""
-    for letter in txt:
-        # index = base.index(letter)  # not used
-        cipher_txt += base[base.index(letter) + code]
+    for item in txt:  # improving to get the multiwords result
+        for letter in item:
+            # index = base.index(letter)  # not used
+            cipher_txt += alphabet[alphabet.index(letter) + code - len(alphabet)]
+        cipher_txt += " "  # multiword connector
     print(f"The encoded text in {cipher_txt}")
+
 
 alphabet = list("a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" "))
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
+text = input("Type your message:\n").lower().split()
 shift = int(input("Type the shift number:\n"))
 
 # 1 Create a foo called 'encrypt that takes the 'text' and 'shift' as inputs.
@@ -22,6 +25,6 @@ shift = int(input("Type the shift number:\n"))
 #   print output: "The encoded text is mjqqt"
 # 3 Call the encrypt foo and pass in the user inputs. You shoout be able to text the code.
 if direction == 'encode':
-    encrypt(alphabet, text, shift)
+    encrypt(text, shift)
 else:
     pass
