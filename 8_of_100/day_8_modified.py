577b9286e5214e9ff183ac68e5dc71f1655c69e4
DesLandysh <denis.schizarium@gmail.com>
2021-08-10 02:49:53 +0300

    Step 4. refactored, and Fixed 'holve' issue

--- 8_of_100/day_8_modified.py
+++ 8_of_100/day_8_modified.py
@@ -1,24 +1,32 @@
 # Caesar Cipher
 
-def cipher(txt, code, sign):
+def caesar(txt, code, sign):
+    """
+    Encode and decode the text by caesar cipher script
+    :param txt: user text
+    :param code: amount of shift
+    :param sign: direction switcher
+    :return: new text
+    """
     cipher_txt = ""
-    for item in txt:  # improving to get the multiwords result
+    if sign == 'decode':
+        code *= -1
+    elif sign != 'decode' and sign != "encode":  # prevent mistyping
+        print("Typed wrong command, Exiting...")
+        return None
+    for item in txt:  # improving to get the multi-words result
         for letter in item:
-            if sign:
-                cipher_txt += alphabet[(alphabet.index(letter) + code) % len(alphabet)]  # stolen variant (... % 26)
-            else:
-                cipher_txt += alphabet[(alphabet.index(letter) - code) % len(alphabet)]
-        cipher_txt += " "  # multiword connector
-    print(f"The encoded text is {cipher_txt}")
+            cipher_txt += alphabet[(alphabet.index(letter) + code) % len(alphabet)]
+        cipher_txt += " "  # multi-word connector
+    print(f"The {sign}d text is {cipher_txt}")
 
 
-alphabet = list("a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" "))
+alphabet = list("a b c d e f g h i j k l m n o p q r s t u v w x y z".split())
 
+# User instructions
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower().split()
 shift = int(input("Type the shift number:\n"))
 
-if direction == 'encode':
-    cipher(text, shift, True)
-elif direction == 'decode':
-    cipher(text, shift, False)
+# request to cipher function
+caesar(text, shift, direction)
