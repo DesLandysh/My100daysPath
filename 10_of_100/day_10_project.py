20fe05f1d8c106ab5415d537df1e648469c142c8
DesLandysh <denis.schizarium@gmail.com>
2021-08-12 14:06:12 +0300

    day 10 calc

--- 10_of_100/day_10_project.py
+++ 10_of_100/day_10_project.py
@@ -1,4 +1,63 @@
 # Calculator
-from day_10_art import logo
+from day_10_art import logo, clear
+
+
+def do_add(n1, n2):
+    return n1 + n2
+
+
+def do_sub(n1, n2):
+    return n1 - n2
+
+
+def do_mul(n1, n2):
+    return n1 * n2
+
+
+def do_div(n1, n2):
+    try:
+        return n1 / n2
+    except ZeroDivisionError:
+        return 0
+
 
 print(logo)
+operators = ["+", "-", "*", "/"]
+program = True
+
+while program:
+    next_op = True
+    result = int(input("Enter the number: "))
+    while next_op:
+        print("Example of operators:")
+        for op in operators:
+            print(f"    {op}")
+        operation = input("Choose an operator: ")
+        number_2 = int(input("Enter the second number: "))
+        if operators[0] == operation:
+            tmp = do_add(result, number_2)
+            print(f"{round(result, 1)} {operation} {number_2} = {round(tmp, 1)}")
+            result = tmp
+        elif operators[1] == operation:
+            tmp = do_sub(result, number_2)
+            print(f"{round(result, 1)} {operation} {number_2} = {round(tmp, 1)}")
+            result = tmp
+        elif operators[2] == operation:
+            tmp = do_mul(result, number_2)
+            print(f"{round(result, 1)} {operation} {number_2} = {round(tmp, 1)}")
+            result = tmp
+        elif operators[3] == operation:
+            tmp = do_div(result, number_2)
+            print(f"{round(result, 1)} {operation} {number_2} = {round(tmp, 1)}")
+            result = tmp
+        else:
+            print("Not allowed operation")
+            continue
+        if input("Will you want to continue math? y/n :").lower() == "n":
+            print(f"\nFinal result of your calculation is {result}\n")
+            next_op = False
+    if input("will you want for a new calculations? y/n :").lower() == "n":
+        program = False
+    clear()
+
+quit()
---
+++ 10_of_100/day_10_project_ver2.py
@@ -0,0 +1,66 @@
+# 100 days of code version and a little modified with dividing on zero and quit
+from day_10_art import logo, clear
+
+
+def do_add(n1, n2):
+    return n1 + n2
+
+
+def do_sub(n1, n2):
+    return n1 - n2
+
+
+def do_mul(n1, n2):
+    return n1 * n2
+
+
+def do_div(n1, n2):
+    try:
+        return n1 / n2
+    except ZeroDivisionError:
+        return 0
+
+
+operations = {
+    "+": do_add,
+    "-": do_sub,
+    "*": do_mul,
+    "/": do_div,
+    }
+
+
+def calculations():
+    clear()
+    print(logo, "\n")
+
+    num1 = float(input("Enter the first number?: "))
+    for key in operations:
+        print(key)
+    should_continue = True
+
+    while should_continue:
+        operator = input("pick an operation: ")
+        num2 = float(input("Enter the next number?: "))
+        calc_function = operations[operator]
+        answer = round(calc_function(num1, num2), 1)
+
+        print(f"{num1} {operator} {num2} = {answer}")
+        """
+        we set dictionary as a switcher key=operator, and value=function
+        calc_function return do_math function by key == operator
+        then answer = we call calc_function which store the value = func from the dictionary by key=operator
+        that's reduces the if/else statements
+        
+        hmm, such clever.
+        """
+        flag = input(f"Type 'y' for continue calculating with {answer},\nor type 'n' for a new calculations,\nor type 'q' for exit: ")
+        if flag == "y":
+            num1 = answer
+        elif flag == 'n':
+            should_continue = False
+            calculations()
+        else:
+            return None
+
+
+calculations()
