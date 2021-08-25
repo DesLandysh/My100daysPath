86271397cbbb980ed904049af24289e20031a243
DesLandysh <denis.schizarium@gmail.com>
2021-08-25 03:09:15 +0300

    I'm ill but it works with no logic

--- 011_of_100/day_11_blackjack_comsole.py
+++ 011_of_100/day_11_blackjack_comsole.py
@@ -7,34 +7,37 @@ def clear():
     print("\n"*20)
 
 
-def deal_the_cards(side):
+def deal_the_cards(hand):
     card = random.randint(0, len(from_deck))
     # print(card)
-    side.append(from_deck.get(deck[card]))
-    return side
+    hand.append(from_deck.get(deck[card]))
+    return hand
 
 
-def shuffle(side):
+def shuffle(hand):
     for i in range(0, 2):
-        deal_the_cards(side)
-    return side
+        deal_the_cards(hand)
+    return hand
 
 
 deck = ("6", "7", "8", "9", "10", "J", "Q", "K", "A")
 from_deck = {"6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 2, "Q": 3, "K": 4, "A": 11}
 is_game = True
-players_cards = []
-comp_cards = []
+result = ""
+players_hand, comp_hand = [], []
+players_hand = shuffle(players_hand)
+comp_hand = shuffle(comp_hand)
 
 while is_game:
     print(logo)
-    players_cards = shuffle(players_cards)
-    comp_cards = shuffle(comp_cards)
-    print(f"Your cards: {players_cards}")
-    print(f"Computer first card: {comp_cards[0]}")
+    print(f"Your cards: {players_hand}")
+    print(f"Computer first card: {comp_hand[0]}")
     if input("do you want to play again?: ").lower() == "y":
+        players_hand, comp_hand = [], []
         print("cont")
     else:
         is_game = False
 
-print("final result")
+print(f"Your final hand: {players_hand}")
+print(f"Computer's final hand: {comp_hand}")
+print(f"{result}")
