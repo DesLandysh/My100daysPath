40b48e849b47c87ed057f72f1a16768e1d351b0a
DesLandysh <denis.schizarium@gmail.com>
2021-08-30 18:36:54 +0300

    Day 16 start oop (delete replit module)

---
+++ 016_of_100/main.py
@@ -0,0 +1,14 @@
+# Coffee Machine OOP ver
+
+# Pre-req code
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+# End pre-req code
+
+# 100days of code has a Terrible documentation and tech writing skill
+
+off = False
+while not off:
+
+    off = True
