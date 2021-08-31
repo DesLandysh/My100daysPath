c0046501958bd4085d272bba2fc505a37f693137
DesLandysh <denis.schizarium@gmail.com>
2021-08-31 21:10:53 +0300

    Day 18 turtle square...

---
+++ 018_of_100/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+des = Turtle()
+des.shape("turtle")
+# des.color("coral")
+for i in range(4):
+    des.right(90)
+    des.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
