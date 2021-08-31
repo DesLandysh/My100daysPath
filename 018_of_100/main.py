33a344d7a10d25748c717f1e344ccd61cc80dec1
DesLandysh <denis.schizarium@gmail.com>
2021-08-31 22:13:37 +0300

    Day 18 turtle .

--- 018_of_100/main.py
+++ 018_of_100/main.py
@@ -1,25 +1,34 @@
 from turtle import Turtle, Screen
-from random import choice
-
-des = Turtle()
-des.shape("turtle")
-colors = ["red", "blue", "black", "coral", "orange", "violet", "magenta", "cyan", "green", "olive"]
-# des.color("coral")
-for i in range(3, 16):
-    des.color(choice(colors))
-    for _ in range(i):
-        des.forward(50)
-        des.right(360/i)
-
-
-
-
-
-
-
-
-
-
+import random
 
 screen = Screen()
+screen.colormode(255)
+des = Turtle()
+# des.shape("turtle")
+colors = ["red", "blue", "black", "coral", "orange", "violet", "magenta", "cyan", "green", "olive"]
+angles = [0, 90, 180, 270]
+des.pensize(10)
+des.speed(10)
+
+def rgb():
+    r = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    return r
+
+def r_dir(ang):
+    des.setheading(ang)
+    des.forward(40)
+
+for i in range(100):
+    des.pencolor(rgb())
+    r_dir(random.choice(angles))
+
+
+
+
+
+
+
+
+
+
 screen.exitonclick()
