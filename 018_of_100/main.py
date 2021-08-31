9d82bb66993531fd60f2beadfb2c481da70ac8db
DesLandysh <denis.schizarium@gmail.com>
2021-08-31 21:45:05 +0300

    Day 18 turtle .

--- 018_of_100/main.py
+++ 018_of_100/main.py
@@ -1,15 +1,15 @@
 from turtle import Turtle, Screen
+from random import choice
 
 des = Turtle()
 des.shape("turtle")
+colors = ["red", "blue", "black", "coral", "orange", "violet", "magenta", "cyan", "green", "olive"]
 # des.color("coral")
-for i in range(50):
-    if i % 2 == 0:
-        des.penup()
-        des.forward(5)
-    des.pendown()
-    des.forward(5)
-
+for i in range(3, 16):
+    des.color(choice(colors))
+    for _ in range(i):
+        des.forward(50)
+        des.right(360/i)
 
 
 
